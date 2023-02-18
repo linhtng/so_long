b017aa86fa01c041c4a049f2eb4efd01b1953434
Linh <33163654+linhtng@users.noreply.github.com>
2023-02-18 21:38:12 +0200

    Add files via upload

--- includes/so_long.h
+++ includes/so_long.h
@@ -19,20 +19,30 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-typedef struct s_map
+typedef struct s_validpath
 {
 	int     lines;
 	int     columns;
     char    **map;
     char    **visited;
     int     collects;
-/*     char    start;
-    char    collect;
-    char    exit; */
-}	t_map;
+    int     new_start_i;
+    int     new_start_j;
+}	t_validpath;
 
 void	clean_exit(char *message);
 void	free_arr(char **arr);
-int	valid_path(char **map, int lines, int columns, int collectibles);
+void	valid_path(char **map, int lines, int columns);
 void	print_arr(char **arr);
+void	malloc_error(char *message, char **mem1, char **mem2);
+int	    count_occurences(char *str, char c);
+int	    count_occurences_arr(char **arr, int lines, char c);
+
+/* so_long_valid_path_utils */
+char	**empty_map(char **map, t_validpath *check_path, int lines, int columns);
+char	**ft_arrdup(char **map, int lines, t_validpath *check_path);
+int	cell_inbound(int i, int j, int lines, int columns);
+void	invalid_path_exit(char **map, t_validpath *check_path, char *message);
+void	reset_visited(t_validpath *check_path);
+
 #endif
