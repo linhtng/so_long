87273ae8b1a6c7162bca18de14d0754a17d078ec
Linh <33163654+linhtng@users.noreply.github.com>
2023-02-21 20:25:29 +0200

    Add files via upload

--- includes/so_long.h
+++ includes/so_long.h
@@ -18,6 +18,9 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <unistd.h>
+# ifndef SIZE
+#  define SIZE 32
+# endif
 
 typedef struct s_validpath
 {
@@ -30,9 +33,44 @@ typedef struct s_validpath
     int     new_start_j;
 }	t_validpath;
 
+typedef struct s_vector
+{
+    int     row;
+    int     col;
+}	t_vector;
+
+typedef struct s_components
+{
+	int         lines;
+	int         columns;
+    int         collectibles;
+    t_vector    player_pos;
+    t_vector    exit_pos;
+}	t_components;
+
+typedef struct s_image
+{
+	void        *reference;
+    t_vector    size;
+}	t_image;
+
+typedef struct s_game
+{
+    void            *mlx;
+    void            *window;
+    t_image         collect;
+    t_image         player;
+    t_image         wall;
+    t_image         exit;
+    char            **map_arr;
+    int             window_height;
+    int             window_width;
+    t_components    game_comps;
+}	t_game;
+
 void	clean_exit(char *message);
 void	free_arr(char **arr);
-void	valid_path(char **map, int lines, int columns);
+void	valid_path(char **map, int lines, int columns, t_components *game_comps);
 void	print_arr(char **arr);
 void	malloc_error(char *message, char **mem1, char **mem2);
 int	    count_occurences(char *str, char c);
@@ -41,7 +79,7 @@ int	    count_occurences_arr(char **arr, int lines, char c);
 /* so_long_valid_path_utils */
 char	**empty_map(char **map, t_validpath *check_path, int lines, int columns);
 char	**ft_arrdup(char **map, int lines, t_validpath *check_path);
-int	cell_inbound(int i, int j, int lines, int columns);
+int	    cell_inbound(int i, int j, int lines, int columns);
 void	invalid_path_exit(char **map, t_validpath *check_path, char *message);
 void	reset_visited(t_validpath *check_path);
 
