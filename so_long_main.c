86f86ad646d61c8aaeaae3bec49cece5472ed9ab
Linh <33163654+linhtng@users.noreply.github.com>
2023-03-06 18:09:07 +0200

    Add files via upload

--- so_long_main.c
+++ so_long_main.c
@@ -6,7 +6,7 @@
 /*   By: thuynguy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 18:13:23 by thuynguy          #+#    #+#             */
-/*   Updated: 2023/02/09 18:16:21 by thuynguy         ###   ########.fr       */
+/*   Updated: 2023/03/05 14:46:35 by thuynguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,8 @@ char	*get_map_string(int fd, t_components *game_comp)
 		free(buf);
 		buf = get_next_line(fd);
 	}
-	return(map_string);
+	close(fd);
+	return (map_string);
 }
 
 char	**get_map_array(char *map_str, t_components *game_comps)
@@ -60,9 +61,11 @@ char	**get_valid_map(int fd, t_components *game_comps)
 	arr_len = 0;
 	map = NULL;
 	map_string = get_map_string(fd, game_comps);
-	arr_len = ft_strlen(map_string) - count_occurences(map_string, '\n');
 	if (map_string)
+	{
+		arr_len = ft_strlen(map_string) - count_occurences(map_string, '\n');
 		map = get_map_array(map_string, game_comps);
+	}
 	else
 		clean_exit("Map is empty.\n");
 	if (map)
@@ -75,7 +78,7 @@ char	**get_valid_map(int fd, t_components *game_comps)
 	return (map);
 }
 
-void	game_comps_init(t_components *game_comps)
+/* void	game_comps_init(t_components *game_comps)
 {
 	game_comps->lines = 0;
 	game_comps->columns = 0;
@@ -84,7 +87,7 @@ void	game_comps_init(t_components *game_comps)
 	game_comps->player_pos.x = 0;
 	game_comps->exit_pos.y = 0;
 	game_comps->exit_pos.x = 0;
-}
+} */
 
 int	main(int argc, char **argv)
 {
@@ -94,11 +97,11 @@ int	main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		fd = open(argv[1], O_RDONLY);	
+		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
 			clean_exit("File cannot be opened.\n");
 		correct_extension(argv[1]);
-		game_comps_init(&game_comps);
+		ft_bzero(&game_comps, sizeof(t_components));
 		map = get_valid_map(fd, &game_comps);
 		if (map)
 			game_init(map, game_comps);
